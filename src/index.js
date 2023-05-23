0225f0eb3a1c26c2d8c7612fdf79f21db2d5947b
Patrick Carter <xCarter93@gmail.com>
2023-05-22 22:28:17 -0400

    Finished project

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import App from "./App";
+import App from "./containers/App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
